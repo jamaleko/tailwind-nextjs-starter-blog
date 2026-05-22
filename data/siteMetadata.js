416c383d7dc40aab2defb2975cc00c4ae92df690
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-22 09:00:59 +0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Portal Berita Sains dan Teknologi',
+  author: 'jamaleko',
+  headerTitle: 'Tekuna',
+  description: 'Tekuna menyajikan berita sains dan teknologi terbaru, inovasi terkini, serta perkembangan dunia digital secara akurat dan informatif.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
