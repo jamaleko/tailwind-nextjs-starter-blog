5056de4005808e54a542c021d4d4f057441f092b
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-23 21:43:59 +0700

    Update page.tsx

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,4 +1,11 @@
+import { allBlogs } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
+
+export async function generateStaticParams() {
+  return allBlogs.map((p) => ({
+    slug: p.slug.split('/').map((name) => decodeURI(name)),
+  }))
+}
 
 export default function Page() {
   notFound()
