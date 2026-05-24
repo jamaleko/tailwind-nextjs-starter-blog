f5d04ee92021c99e5018d74768ce14b2aa8fd413
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-24 17:06:52 +0700

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,7 +1,6 @@
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
-import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const MAX_DISPLAY = 5
@@ -10,28 +9,24 @@ export default function Home({ posts }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        {/*<div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
-        </div>*/}
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
+
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
+            const { slug, title, summary, tags, images } = post
+
             return (
               <li key={slug} className="py-12">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-center xl:gap-6 xl:space-y-0">
+                    <div className="hidden md:block">
+                      <img
+                        src={images?.[0] || '/static/images/avatar.png'}
+                        alt={title}
+                        className="h-32 w-full rounded-lg object-cover"
+                      />
+                    </div>
+
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
@@ -43,16 +38,19 @@ export default function Home({ posts }) {
                               {title}
                             </Link>
                           </h2>
+
                           <div className="flex flex-wrap">
                             {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}
                           </div>
                         </div>
+
                         <div className="prose max-w-none text-gray-500 dark:text-gray-400">
                           {summary}
                         </div>
                       </div>
+
                       <div className="text-base leading-6 font-medium">
                         <Link
                           href={`/berita/${slug}`}
@@ -70,6 +68,7 @@ export default function Home({ posts }) {
           })}
         </ul>
       </div>
+
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base leading-6 font-medium">
           <Link
@@ -81,6 +80,7 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
+
       {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
